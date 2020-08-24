0162f2789b5e3f9b8baa72e6d65835754de4d056
aaravr <>
2020-08-24 16:53:52 +0100

    @LME0000 @RE @RE Martial robot navigation initial version[WIP]

--- src/test/java/com/aaravr/martian/robot/navigator/service/ControlCenterTest.java
+++ src/test/java/com/aaravr/martian/robot/navigator/service/ControlCenterTest.java
@@ -30,14 +30,14 @@ public class ControlCenterTest {
     }
 
     @Test
-    public void should_move_back_original_position_after_round_trip() throws Exception {
+    public void should_move_back_to_original_position_after_round_trip() throws Exception {
         sut.initialiseMarsSurface("5 3");
         Optional<Robot> robot = sut.validateAndProcessRobotMovementInput("3 2 W", "FFFLFLFFFLF");
         Assert.assertThat(getPositionString(robot.get(), false), is("3 2 N"));
     }
 
     @Test
-    public void should_turn_360_clockwise() throws Exception {
+    public void should_turn_360_degrees_clockwise() throws Exception {
         sut.initialiseMarsSurface("5 3");
         Optional<Robot> robot = sut.validateAndProcessRobotMovementInput("3 2 N", "R");
         Assert.assertThat(getPositionString(robot.get(), false), is("3 2 E"));
@@ -53,7 +53,7 @@ public class ControlCenterTest {
     }
 
     @Test
-    public void should_turn_360_anti_clockwise() throws Exception {
+    public void should_turn_360_degrees_anti_clockwise() throws Exception {
         sut.initialiseMarsSurface("5 3");
         Optional<Robot> robot = sut.validateAndProcessRobotMovementInput("3 2 N", "L");
         Assert.assertThat(getPositionString(robot.get(), false), is("3 2 W"));
@@ -69,14 +69,14 @@ public class ControlCenterTest {
     }
 
     @Test
-    public void should_jump_off_the_grid_and_report_position() throws Exception {
+    public void should_jump_off_the_grid_and_report_last_occupied_position() throws Exception {
         sut.initialiseMarsSurface("5 3");
         Optional<Robot> robot = sut.validateAndProcessRobotMovementInput("3 2 N", "FRRFLLFFRRFLL");
         Assert.assertThat(getPositionString(robot.get(), true), is("3 3 N LOST"));
     }
 
     @Test
-    public void should_ignore_moving_forward_from_previous_scented_position() throws Exception {
+    public void should_ignore_move_forward_command_for_scented_position() throws Exception {
         sut.initialiseMarsSurface("5 3");
         sut.validateAndProcessRobotMovementInput("3 2 N", "FRRFLLFFRRFLL");
         Optional<Robot> robot = sut.validateAndProcessRobotMovementInput("3 2 N", "FRRF");
@@ -84,12 +84,12 @@ public class ControlCenterTest {
     }
 
     @Test(expected = InvalidInputException.class)
-    public void should_throw_exception_for_invalid_surface_coordinates() throws Exception {
+    public void should_throw_exception_for_surface_coordinates_exceed_max_limit() throws Exception {
         sut.initialiseMarsSurface("50 3");
     }
 
     @Test(expected = InvalidInputException.class)
-    public void should_throw_exception_for_off_grid_starting_position() throws Exception {
+    public void should_throw_exception_for_off_grid_robot_starting_position() throws Exception {
         sut.initialiseMarsSurface("49 3");
         sut.validateAndProcessRobotMovementInput("50 1 E", "RFRFRFRF");
     }
